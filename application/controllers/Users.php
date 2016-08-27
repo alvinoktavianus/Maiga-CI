c8b58376c18fd01e9e6e8218c1168113424f0cf1
Alvin Oktavianus <agentsam1995@gmail.com>
2016-08-27 17:54:39 +0700

    Configure routes

---
+++ application/controllers/Users.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Users extends CI_Controller {
+
+	public function index()
+	{
+		echo "index";
+	}
+
+	public function do_login()
+	{
+		echo "do login";
+	}
+
+}
+
+/* End of file Users.php */
+/* Location: ./application/controllers/Users.php */
