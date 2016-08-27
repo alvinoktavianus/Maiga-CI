fe37598182b3359a6adaeb9887a56770e0478819
Alvin Oktavianus <agentsam1995@gmail.com>
2016-08-28 04:12:26 +0700

    Set configuration for mis.maiga.co.id

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8888/Maiga-CI/';
+$config['base_url'] = 'http://mis.maiga.co.id/';
 
 /*
 |--------------------------------------------------------------------------
