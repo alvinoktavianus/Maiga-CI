28f2b0c9a00d285cad72dcfd061e13d2bfa6bfce
Alvin Oktavianus <agentsam1995@gmail.com>
2016-09-02 11:01:55 +0700

    Update admin layout
    Add some markup to admin layout, currently not all layouts has been updated

--- application/views/layouts/admin/employeelist.php
+++ application/views/layouts/admin/employeelist.php
@@ -1,44 +1,55 @@
-<?php if (count($employees) > 0): ?>
-
-<h2 class="text-center">Daftar Karyawan</h2>
-<br>
-<?php if ($this->session->flashdata('success')): ?>
-    <div class="alert alert-success" role="alert">
-        <strong><?php echo $this->session->flashdata('success'); ?></strong>
+<article class="content responsive-tables-page">
+    <div class="title-block">
+        <h1 class="title">Daftar Karyawan</h1>
+        <p class="title-description"> Maiga Corp. </p>
     </div>
-<?php endif; ?>
-<div class="table-responsive">
-    <table class="table table-bordered table-striped">
-        <thead>
-        <tr>
-            <th>Nama Karyawan</th>
-            <th>Email</th>
-            <th>Jabatan</th>
-            <th>Departemen</th>
-            <th colspan="3">Status</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php foreach($employees as $employee): ?>
-        	<tr>
-        		<td><?php echo $employee->nama; ?></td>
-        		<td><?php echo $employee->email; ?></td>
-        		<td><?php echo $employee->jabatan; ?></td>
-        		<td><?php echo $employee->department; ?></td>
-        		<td><?php echo $employee->status; ?></td>
-        		<td><a href="<?php echo base_url(); ?>admin/updateemployee?email=<?php echo $employee->email; ?>">Update Employee</a></td>
-        		<td>
-        			<?php echo form_open(base_url().'admin/removeemployee?email='.$employee->email); ?>
-    				<?php echo form_submit('remove', 'Remove', array( 'class' => 'btn btn-danger btn-xs' )); ?>
-        			<?php echo form_close(); ?>
-        		</td>
-        	</tr>
-        <?php endforeach; ?>
-        </tbody>
-    </table>
-</div>
-<?php else: ?>
+    <section class="section">
 
-	<h4>No employee</h4>
+        <div class="card card-block">
 
-<?php endif; ?>
+            <?php if (count($employees) > 0): ?>
+
+            <?php if ($this->session->flashdata('success')): ?>
+                <div class="alert alert-success" role="alert">
+                    <strong><?php echo $this->session->flashdata('success'); ?></strong>
+                </div>
+            <?php endif; ?>
+            <div class="table-responsive">
+                <table class="table table-bordered table-striped">
+                    <thead>
+                    <tr>
+                        <th>Nama Karyawan</th>
+                        <th>Email</th>
+                        <th>Jabatan</th>
+                        <th>Departemen</th>
+                        <th colspan="3">Status</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php foreach($employees as $employee): ?>
+                    	<tr>
+                    		<td><?php echo $employee->nama; ?></td>
+                    		<td><?php echo $employee->email; ?></td>
+                    		<td><?php echo $employee->jabatan; ?></td>
+                    		<td><?php echo $employee->department; ?></td>
+                    		<td><?php echo $employee->status; ?></td>
+                    		<td><a href="<?php echo base_url(); ?>admin/updateemployee?email=<?php echo $employee->email; ?>">Update Employee</a></td>
+                    		<td>
+                    			<?php echo form_open(base_url().'admin/removeemployee?email='.$employee->email); ?>
+                				<?php echo form_submit('remove', 'Remove', array( 'class' => 'btn btn-danger btn-xs' )); ?>
+                    			<?php echo form_close(); ?>
+                    		</td>
+                    	</tr>
+                    <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+            <?php else: ?>
+
+            	<h4>No employee</h4>
+
+            <?php endif; ?>
+
+        </div>
+    </section>
+</article>
--- application/views/layouts/admin/home.php
+++ application/views/layouts/admin/home.php
@@ -1,5 +1,17 @@
-<div class="alert alert-success" role="alert">
-    <p class="text-center">
-        <strong>Welcome, </strong><?php echo $this->session->userdata('user_session')['email']; ?>
-    </p>
-</div>
+
+<article class="content responsive-tables-page">
+    <div class="title-block">
+        <h1 class="title">Home</h1>
+        <p class="title-description"> Maiga Corp. </p>
+    </div>
+    <section class="section">
+
+        <div class="card card-block">
+            <div class="alert alert-success" role="alert">
+                <p class="text-center">
+                    <strong>Welcome, </strong><?php echo $this->session->userdata('user_session')['email']; ?>
+                </p>
+            </div>
+        </div>
+    </section>
+</article>
