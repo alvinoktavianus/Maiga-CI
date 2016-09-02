01a735a96e62e050cb61a31ce1c04b3001964925
Alvin Oktavianus <agentsam1995@gmail.com>
2016-09-02 11:35:06 +0700

    Update manager layout
    Add some markup to manager layout, currently not all layouts has been updated

--- application/views/layouts/manager/checkassignments.php
+++ application/views/layouts/manager/checkassignments.php
@@ -1,98 +1,110 @@
-<h2 class="text-center">Check Assignment</h2>
-<br>
-
-<form action="<?php echo base_url(); ?>manager/checkassignments" class="form-horizontal" enctype="multipart/form-data" method="get" accept-charset="utf-8">
-
-<div class="row">
-
-    <div class="col-md-6 col-md-offset-3">
-
-        <div class="form-group">
-            <?php echo form_label('Pilih Topik', 'topic', array( 'class' => 'col-sm-4 control-label' )); ?>
-            <div class="col-sm-8">
-                <?php echo form_dropdown('topic', $topics, $topic, array( 'class' => 'form-control', 'required' => true )); ?>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <p class="text-center">
-                <?php echo form_submit('', 'Pilih', array( 'class' => 'btn btn-success' )); ?>
-            </p>
-        </div>
-
+<article class="content responsive-tables-page">
+    <div class="title-block">
+        <h1 class="title">Check Assignment</h1>
+        <p class="title-description"> Maiga Corp. </p>
     </div>
+    <section class="section">
 
-</div>
+        <div class="card card-block">
 
-<?php echo form_close(); ?>
+            <form action="<?php echo base_url(); ?>manager/checkassignments" class="form-horizontal" enctype="multipart/form-data" method="get" accept-charset="utf-8">
+            <?php echo form_hidden($this->security->get_csrf_token_name(), $this->security->get_csrf_hash()); ?>
+            <div class="row">
 
-<?php if (count($assignments) > 0): ?>
+                <div class="col-md-6 col-md-offset-3">
 
-<table class="table table-bordered table-striped">
-    <thead>
-        <th>Email</th>
-        <th>Nama Karyawan</th>
-        <th>Deskripsi</th>
-        <th>File Tugas</th>
-        <th>Tanggal Upload</th>
-        <th>Tanggal Update</th>
-        <th></th>
-        <th></th>
-    </thead>
-    <tbody>
-        <?php foreach($assignments as $assignment): ?>
-            <tr>
-                <td><?php echo $assignment->email; ?></td>
-                <td><?php echo $assignment->nama; ?></td>
-                <td><?php echo nl2br($assignment->description); ?></td>
-                <td><a target="_blank" href="<?php echo base_url(); ?>manager/downloadassignment?filename=<?php echo $assignment->assignment; ?>"><?php echo $assignment->assignment; ?></a></td>
-                <td><?php echo date("D, d M Y | H:i", strtotime($assignment->createdttm)); ?></td>
-                <td><?php if ( $assignment->updatedttm != null ) echo date("D, d M Y | H:i", strtotime($assignment->updatedttm)); ?></td>
-                <td>
-                    <?php 
-                        if ( $assignment->status == 'P' ) {
-                            echo form_open(base_url().'manager/updateassignmentstatus?email='.$assignment->email.'&filename='.$assignment->assignment.'&status=A&topic='.$this->input->get('topic'));
-                            echo form_submit('', 'Approve', array( 'class' => 'btn btn-success btn-xs' ));
-                            echo form_close();
-                        }
-                        else {
-                            switch ($assignment->status) {
-                                case 'A':
-                                    echo 'Approved';
-                                    break;
-                                case 'C':
-                                    echo 'Canceled';
-                                    break;
-                            }
-                        }
-                    ?>
-                </td>
-                <td>
-                    <?php 
-                        if ( $assignment->status == 'P' ) {
-                            echo form_open(base_url().'manager/updateassignmentstatus?email='.$assignment->email.'&filename='.$assignment->assignment.'&status=C&topic='.$this->input->get('topic'));
-                            echo form_submit('', 'Cancel', array( 'class' => 'btn btn-danger btn-xs' ));
-                            echo form_close();
-                        }
-                        else {
-                            switch ($assignment->status) {
-                                case 'A':
-                                    echo 'Approved';
-                                    break;
-                                case 'C':
-                                    echo 'Canceled';
-                                    break;
-                            }
-                        }
-                    ?>
-                </td>
-            </tr>
-        <?php endforeach; ?>
-    </tbody>
-</table>
+                    <div class="form-group">
+                        <?php echo form_label('Pilih Topik', 'topic', array( 'class' => 'col-sm-4 control-label' )); ?>
+                        <div class="col-sm-8">
+                            <?php echo form_dropdown('topic', $topics, $topic, array( 'class' => 'form-control', 'required' => true )); ?>
+                        </div>
+                    </div>
 
-<?php else: ?>
+                    <div class="form-group">
+                        <p class="text-center">
+                            <?php echo form_submit('', 'Pilih', array( 'class' => 'btn btn-success' )); ?>
+                        </p>
+                    </div>
 
-<h4>No assignments</h4>
+                </div>
 
-<?php endif; ?>
+            </div>
+
+            <?php echo form_close(); ?>
+
+            <?php if (count($assignments) > 0): ?>
+
+            <table class="table table-bordered table-striped">
+                <thead>
+                    <th>Email</th>
+                    <th>Nama Karyawan</th>
+                    <th>Deskripsi</th>
+                    <th>File Tugas</th>
+                    <th>Tanggal Upload</th>
+                    <th>Tanggal Update</th>
+                    <th></th>
+                    <th></th>
+                </thead>
+                <tbody>
+                    <?php foreach($assignments as $assignment): ?>
+                        <tr>
+                            <td><?php echo $assignment->email; ?></td>
+                            <td><?php echo $assignment->nama; ?></td>
+                            <td><?php echo nl2br($assignment->description); ?></td>
+                            <td><a target="_blank" href="<?php echo base_url(); ?>manager/downloadassignment?filename=<?php echo $assignment->assignment; ?>"><?php echo $assignment->assignment; ?></a></td>
+                            <td><?php echo date("D, d M Y | H:i", strtotime($assignment->createdttm)); ?></td>
+                            <td><?php if ( $assignment->updatedttm != null ) echo date("D, d M Y | H:i", strtotime($assignment->updatedttm)); ?></td>
+                            <td>
+                                <?php 
+                                    if ( $assignment->status == 'P' ) {
+                                        echo form_open(base_url().'manager/updateassignmentstatus?email='.$assignment->email.'&filename='.$assignment->assignment.'&status=A&topic='.$this->input->get('topic'));
+                                        echo form_submit('', 'Approve', array( 'class' => 'btn btn-success btn-xs' ));
+                                        echo form_close();
+                                    }
+                                    else {
+                                        switch ($assignment->status) {
+                                            case 'A':
+                                                echo 'Approved';
+                                                break;
+                                            case 'C':
+                                                echo 'Canceled';
+                                                break;
+                                        }
+                                    }
+                                ?>
+                            </td>
+                            <td>
+                                <?php 
+                                    if ( $assignment->status == 'P' ) {
+                                        echo form_open(base_url().'manager/updateassignmentstatus?email='.$assignment->email.'&filename='.$assignment->assignment.'&status=C&topic='.$this->input->get('topic'));
+                                        echo form_submit('', 'Cancel', array( 'class' => 'btn btn-danger btn-xs' ));
+                                        echo form_close();
+                                    }
+                                    else {
+                                        switch ($assignment->status) {
+                                            case 'A':
+                                                echo 'Approved';
+                                                break;
+                                            case 'C':
+                                                echo 'Canceled';
+                                                break;
+                                        }
+                                    }
+                                ?>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+
+            <?php else: ?>
+
+            <h4>No assignments</h4>
+
+            <?php endif; ?>
+
+
+        </div>
+
+    </section>
+</article>
